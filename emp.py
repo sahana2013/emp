afbac4b70b5c4a9d25160ec7038b99179d1a6239
Sahana.K <01fe24bca257@kletech.ac.in>
2025-11-26 13:52:30 +0530

    employee salary

--- emp.py
+++ emp.py
@@ -3,11 +3,13 @@ def format_employee_info(name: str, emp_id: int, department: str, salary: float)
     Accepts employee details and returns a formatted string.
     """
     return (
+
         f"Employee Name: {name}\n"
         f"Employee ID: {emp_id}\n"
         f"Department: {department}\n"
-        f"Salary: ₹{salary:,.2f}"
-    )
+        f"Salary: {salary}"
 
-if __name__ == "__main__":
+    )
+# Main block
+if __name__ == "_main_":
     print(format_employee_info("John Doe", 181, "XY", 6789))
