d9c70a2b202934bdd3619574118e821572e7d64b
Sahana.K <01fe24bca257@kletech.ac.in>
2025-11-26 14:12:31 +0530

    employee salary

--- emp.py
+++ emp.py
@@ -1,15 +1,14 @@
-def format_employee_info(name: str, emp_id: int, department: str, salary: float) -> str:
-    """
-    Accepts employee details and returns a formatted string.
-    """
-    return (
-
-        f"Employee Name: {name}\n"
-        f"Employee ID: {emp_id}\n"
-        f"Department: {department}\n"
-        f"Salary: {salary}"
-
+import pytest
+from emp import format_employee_info
+def test_get_employee_info():
+    name = "John Doe"
+    emp_id = "181"
+    department ="XY"
+    salary = 6789
+    expected_output =(
+        "Employee Name: John Doe\n"
+        "Employee ID: 181\n"
+        "Department: XY\n"
+        "Salary: 6789"
     )
-# Main block
-if __name__ == "_main_":
-    print(format_employee_info("John Doe", 181, "XY", 6789))
+    assert format_employee_info(name,emp_id,department,salary) == expected_output
